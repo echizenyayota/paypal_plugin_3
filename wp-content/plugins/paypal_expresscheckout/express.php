b1d6e4148093dc7ac29a217def38aca96d71d175
echizenyayota <virtualmisso30@gmail.com>
2018-06-08 07:43:38 +0900

    Client Side Express Checkout using RESTを記述

--- wp-content/plugins/paypal_expresscheckout/express.php
+++ wp-content/plugins/paypal_expresscheckout/express.php
@@ -21,6 +21,34 @@ add_action( 'wp_enqueue_scripts', 'paypal_scripts' );
 
 // ショートコードとオプションによるPayPalボタンの表示
 function paypaldiv_func(){
+  $paypaldiv = '<div id="paypal-button-container"></div>';
+  $paypaldiv .= "<script>
+		paypal.Button.render({
+			env: 'sandbox',
+			client: {
+				'AZDxjDScFpQtjWTOUtWKbyN_bDt4OgqaF4eYXlewfBP4-8aqX3PiV8e1GWU6liB2CUXlkA59kJXE7M6R',
+			},
+			style: {
+				color: 'blue',
+				size: 'small',
+			},
+			commit: true,
+			payment: function(data, actions) {
+				return actions.payment.create({
+					payment: {
+						transactions: [{
+							amount: { total: '1000', currency: 'JPY' }
+						}]
+					}
+				});
+			},
+			onAuthorize: function(data, actions) {
+				return actions.payment.execute().then(function() {
+					window.alert('Payment Complete!');
+				});
+			}
+		}, '#paypal-button-container');
+	</script>";
 
   // スクリプトの記述が表示される
   return $paypaldiv;
