f2580b0bcc4dcd88078f14d4b76e214cd419abb7
echizenyayota <virtualmisso30@gmail.com>
2018-06-14 14:37:54 +0900

    Client ID入力欄の作成

--- wp-content/plugins/paypal_expresscheckout/express_admin.php
+++ wp-content/plugins/paypal_expresscheckout/express_admin.php
@@ -2,7 +2,16 @@
 
   <div class="wrap">
     <h2>PayPal ExpressCheckout Settings Page</h2>
-
+    <form method="post" action="options.php">
+      <?php settings_fields( 'paypal-settings-group' ); ?>
+      <?php do_settings_sections( 'paypal-settings-group' ); ?>
+      <table class="form-table">
+        <tr valign="top">
+        <th scope="row">Client ID</th>
+        <td><input type="text" name="client" value="<?php echo esc_attr( get_option('client') ); ?>" /></td>
+        </tr>
+      </table>
+    </form>
     <?php submit_button(); ?>
   </div>
 
